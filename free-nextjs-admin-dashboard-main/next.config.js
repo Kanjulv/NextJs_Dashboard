755ec624d91758b6414e62fb08eb86912b4844de
Kanjulv <118467727+Kanjulv@users.noreply.github.com>
2024-08-17 11:20:58 +0530

    .

--- free-nextjs-admin-dashboard-main/next.config.js
+++ free-nextjs-admin-dashboard-main/next.config.js
@@ -1,7 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  basePath: isProd ? '/free-nextjs-admin-dashboard-main' : '',
-  assetPrefix: isProd ? '/free-nextjs-admin-dashboard-main/' : '',
   output: 'export',
 };
